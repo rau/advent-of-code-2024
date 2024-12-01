0c5cc9a1714e5c435c1ac7c28b952b954559f1e1
Raunak Daga <rdaga@college.harvard.edu>
2024-12-01 12:50:22 -0500

    feat: day1 part2

--- 1/day1.py
+++ 1/day1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # part 1
 
 with open("input_1.txt", "r") as file:
@@ -15,5 +17,22 @@ list2 = sorted(list2)
 sum_ = 0
 for pair in zip(list1, list2):
     sum_ += abs(int(pair[1]) - int(pair[0]))
+
+print(sum_)
+
+# part 2
+with open("input_1.txt", "r") as file:
+    data = file.read()
+    data = data.split("   ")
+    list1, list2 = [data[0]], [data[-1]]
+    for i in range(1, len(data) - 1):
+        item1, item2 = data[i].split("\n")
+        list1.append(item2)
+        list2.append(item1)
+
+sum_ = 0
+count = Counter(list2)
+for num in list1:
+    sum_ += int(num) * int(count[num])
 
 print(sum_)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advent-of-code-2024
+
 advent of code solutions
+
+Solved without the use of AI (at least till now :D).
