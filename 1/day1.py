abe70b088edf6a46c8326101df96ca944efb0b49
Raunak Daga <rdaga@college.harvard.edu>
2024-12-01 12:32:15 -0500

    feat: day 1 part 1

---
+++ 1/day1.py
@@ -0,0 +1,19 @@
+# part 1
+
+with open("input_1.txt", "r") as file:
+    data = file.read()
+    data = data.split("   ")
+    list1, list2 = [data[0]], [data[-1]]
+    for i in range(1, len(data) - 1):
+        item1, item2 = data[i].split("\n")
+        list1.append(item2)
+        list2.append(item1)
+
+list1 = sorted(list1)
+list2 = sorted(list2)
+
+sum_ = 0
+for pair in zip(list1, list2):
+    sum_ += abs(int(pair[1]) - int(pair[0]))
+
+print(sum_)
